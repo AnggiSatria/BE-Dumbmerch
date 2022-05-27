c5227c17b70d9c3a4a7901ab3aa29c0d87450073
AnggiSatria <anggisatria122@gmail.com>
2022-05-27 14:56:12 +0700

    update4

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ const PORT = 5000;
 
 app.use(express.json())
 
+app.use("/api/v1", router)
 
-app.listen(PORT, () => console.log(`server running on port : ${PORT}`))
+app.listen(PORT, () => console.log(`server running on port : ${PORT}!`))
