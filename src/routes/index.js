eceb851a05f7279f97a4884302acf8e4b6219143
AnggiSatria <anggisatria122@gmail.com>
2022-05-27 14:36:18 +0700

    update3

--- src/routes/index.js
+++ src/routes/index.js
@@ -30,11 +30,6 @@ const {
   deleteCategory,
 } = require('../controllers/category');
 const { getProfile } = require('../controllers/profile');
-const { register, login, checkAuth } = require('../controllers/auth');
-
-// Middleware
-const { auth } = require('../middlewares/auth');
-const { uploadFile } = require('../middlewares/uploadFile');
 
 // Route
 router.post('/user', addUsers);
@@ -43,16 +38,16 @@ router.get('/user/:id', getUser);
 router.delete('/user/:id', updateUser);
 router.delete('/user/:id', deleteUser);
 
-router.get('/profile', auth, getProfile);
+router.get('/profile', getProfile);
 
-router.get('/products', auth, getProducts);
-router.get('/product/:id', auth, getProduct);
-router.post('/product', auth, uploadFile('image'), addProduct);
-router.patch('/product/:id', auth, uploadFile('image'), updateProduct);
-router.delete('/product/:id', auth, deleteProduct);
+router.get('/products', getProducts);
+router.get('/product/:id', getProduct);
+router.post('/product', addProduct);
+router.patch('/product/:id', updateProduct);
+router.delete('/product/:id', deleteProduct);
 
-router.get('/transactions', auth, getTransactions);
-router.post('/transaction', auth, addTransaction);
+router.get('/transactions', getTransactions);
+router.post('/transaction', addTransaction);
 
 router.post('/notification', notification);
 
@@ -62,8 +57,5 @@ router.post('/category', addCategory);
 router.patch('/category/:id', updateCategory);
 router.delete('/category/:id', deleteCategory);
 
-router.post('/register', register);
-router.post('/login', login);
-router.get('/check-auth', auth, checkAuth);
 
 module.exports = router;
